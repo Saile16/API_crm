da27a62fa5072504c1f6fed8348378f9407f68b5
Alexander Lucano <alrlucano@gmail.com>
2022-05-27 22:42:53 -0500

    Agregamos estilos con tailwind

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,10 +2,30 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>
-      <h1>Desde Layouts</h1>
+    <div className="md:flex md:min-h-screen">
+      <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">
+          CMR - Clientes
+        </h2>
+        <nav className="mt-10">
+          <a
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            href="/clientes"
+          >
+            Clientes
+          </a>
+          <a
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            href="/clientes/nuevo"
+          >
+            Nuevo Cliente
+          </a>
+        </nav>
+      </div>
       {/* revisar esto repasar eloutlet vendria a ser en este caso Inicio mirar rutas*/}
-      <Outlet />
+      <div className="md:w-3/4">
+        <Outlet />
+      </div>
     </div>
   );
 };
